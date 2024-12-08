b510042b5ca5ff5cc2cd05e79a32e615709d5725
Ethan Havemann <echavemann@gmail.com>
2024-12-07 22:35:43 -0600

    Improve Kernel Panic maybe

--- src/user_programs/user_program_exception.cpp
+++ src/user_programs/user_program_exception.cpp
@@ -43,11 +43,6 @@ void exception_task(void)
     set_fault_handler(fault_handler);
 
     trigger_faults();
-    start_timer(callback, 100000, true);
-    auto id2 = start_timer(callback2, 750000, true);
-    cancel_timer(id2);
-
-    set_led(2, 2, true);
 
     while (1) {
         yield();
