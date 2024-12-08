b759d286732af7b617d7277cb5734f89bdd58fe2
Ethan Havemann <echavemann@gmail.com>
2024-12-07 23:10:24 -0600

    Finish Button Code

--- src/user_programs/user_program_sensor.cpp
+++ src/user_programs/user_program_sensor.cpp
@@ -5,12 +5,14 @@
 
 static void button_callback(edge::drivers::ButtonType type, edge::drivers::ButtonState state)
 {
+    edge::userlib::debug_print("button A pressed.\n");
 }
 
 
 void sensor_task(void)
 {
+    edge::userlib::get_button_pressed(edge::drivers::ButtonType::A, button_callback);
     while (1) {
-        edge::userlib::get_button_pressed(edge::drivers::ButtonType::A, button_callback);
+        edge::userlib::yield();
     }
 }
