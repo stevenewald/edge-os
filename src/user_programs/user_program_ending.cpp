6fe0c8aa8f607f04024981935d13d5fd299339c6
stevenewald <stevenewald6@gmail.com>
2024-12-09 21:00:01 -0600

    Improved ending task

--- src/user_programs/user_program_ending.cpp
+++ src/user_programs/user_program_ending.cpp
@@ -1,18 +1,8 @@
 #include "userlib/syscalls.hpp"
-
-#include <cstdio>
+#include "nrf_delay.h"
 
 void ending_task(void)
 {
-    using namespace edge::userlib;
-    volatile int i = 0;
-    bool enabled = true;
-    while (i < 5'000'000) {
-        if (i % 1'000'000 == 0) {
-            edge::userlib::set_led(0, 0, enabled);
-            enabled = !enabled;
-        }
-        i = i + 1;
-    }
-    debug_print("Ending Task: Done Running. 0,0 should stop changing.\n");
+	nrf_delay_ms(1000);
+	edge::userlib::debug_print("Ending Task: Done Running\n");
 }
